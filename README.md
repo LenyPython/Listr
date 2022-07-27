92e61383f245ae8c4fbac435c3ad914986de69fa
Piotr Lenartowicz <piotr.lenartowicz@yahoo.com>
2022-07-27 22:38:50 +0200

    First project setup

---
+++ README.md
@@ -0,0 +1,1 @@
+A attempt to create a jira like to do tables with backend
---
+++ backend/src/main/java/lenypython/Listr/ListrApplication.java
@@ -0,0 +1,13 @@
+package lenypython.Listr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListrApplication.class, args);
+	}
+
+}
